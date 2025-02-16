11b48ddd48ea8d0af0b68ce9ba3a0429db81df46
Andrés Bonilla <x.andresbonilla@gmail.com>
2025-02-16 01:18:44 -0300

    Create migrations for coupons and user_coupons tables with relationships and constraints

---
+++ back/database/migrations/2025_02_16_022917_create_user_coupons_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_coupons', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('coupon_id')->constrained()->onDelete('cascade');
+            $table->string('code')->unique();
+            $table->enum('usage_status', ['used', 'redeemed', 'invalid']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_coupons');
+    }
+};
