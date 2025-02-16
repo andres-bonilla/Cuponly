93afc78119e97f803f2fd1c6ae30d1dc5681ed17
Andrés Bonilla <x.andresbonilla@gmail.com>
2025-02-16 01:07:01 -0300

    Implement Coupon and UserCoupon models with enums, relationships and data casting

---
+++ back/app/Models/Coupon.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+enum CouponStatus: string {
+  case VALID = 'valid';
+  case DRAINED = 'drained';
+  case EXPIRED = 'expired';
+}
+
+class Coupon extends Model {
+  protected $fillable = [
+    'brand',
+    'discount',
+    'status',
+    'expiration',
+    'usage_period'
+  ];
+
+  protected function casts(): array 
+  {
+    return [
+      'status' => CouponStatus::class,
+      'discount' => 'integer',
+      'expiration' => 'datetime',
+      'usage_period' => 'integer',
+    ];
+  }
+
+  public function users() {
+    return $this->belongsToMany(User::class, 'user_coupons')
+        ->withPrivot('usage_status', 'code')
+        ->withTimestamps();
+  }
+}
--- back/app/Models/User.php
+++ back/app/Models/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function coupons() {
+        return $this->belongsToMany(Coupon::class, 'user_coupons')
+            ->withPrivot('usage_status', 'code')
+            ->withTimestamps();
+    }
 }
