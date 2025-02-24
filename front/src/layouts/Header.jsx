3362dc15a8016c07d4908e97afbbbba2c8104967
Andrés Bonilla <x.andresbonilla@gmail.com>
2025-02-24 02:52:13 -0300

    Add layouts and Grid

---
+++ front/src/layouts/Header.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import { Dash } from '../components/Dash'
+import UserLogo from "../assets/user.svg?react";
+import { useNotifier } from '../context/NotifierContext';
+
+export const Header = () => {
+  const [openDash, setOpenDash] = useState(false)
+
+  const { event, trigger } = useNotifier();
+  const acceptedEvents = ["use-coupon-without-login"]
+
+  useEffect(()=>{
+    if (acceptedEvents.includes(event)){
+      setOpenDash(true)
+      trigger("");
+    }
+  },[event])
+
+  return <header id="head">
+      <h1 id='text-logo'>Cupon<span className='text-with-color'>ly</span></h1>
+      <button className='user-button' onClick={() => setOpenDash(prev => !prev)}>
+        <UserLogo id="user-logo"/>
+      </button>
+
+      <Dash openDash={openDash}/>
+    </header>
+}
